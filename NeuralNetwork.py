208ff7ffbdbd709e28810821e6fc4bec47d1849c
Oleksii Galganov <alex.galganov@gmail.com>
2020-10-03 19:39:32 +0300

    vectorized update

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -35,7 +35,7 @@ class Network:
             ind_start += (ind_end - ind_start)
             ind_end += (ind_end - ind_start)
 
-    def h(self, x):
+    def predict(self, x):
         """
         Compute output for input vector x using forward propagation.
         
@@ -46,77 +46,76 @@ class Network:
             a = sigmoid(self.Theta[j-1] @ np.append(1, a))
         return a
 
-    def Cost(self, X, y, lmb = 0):
+    def Cost(self, X, y, Regularization = 0):
         """
         Compute cost function for inputs matrix X and correct outputs y.
         X and y must be numpy arrays w/ shapes (m, n) and (m, 1) respectively.
 
-        lmb (float)     - regularization parameter.
+        Regularization (float)     - regularization parameter.
 
         Returned value is float.
         """
         m, K = len(y), self.Layers[-1]
         J = -1/m * sum([
             sum([
-                y[i][k]*(np.log(self.h(X[i]))[k]) + (1-y[i][k])*(np.log(1-self.h(X[i]))[k])
+                y[i][k]*(np.log(self.predict(X[i]))[k]) + (1-y[i][k])*(np.log(1-self.predict(X[i]))[k])
                 for k in range(K)
             ]) for i in range(m) 
         ])
         T = self.Theta.copy()
         for t in T:
             t[:,0] = 0
-        J += lmb/(2*m) * sum([np.linalg.norm(t, ord = "fro")**2 for t in T])
+        J += Regularization/(2*m) * sum([np.linalg.norm(t, ord = "fro")**2 for t in T])
         return J
 
-    def Grad(self, X, y, lmb = 0):
+    def Grad(self, X, y, Regularization = 0):
         """
         Compute gradient of cost function for inputs matrix X and correct outputs y
         using back propagation.
         X and y must be numpy arrays w/ shapes (m, n) and (m, 1) respectively.
 
-        lmb (float)     - regularization parameter.
+        Regularization (float)     - regularization parameter.
 
         Returned value is in vector form.
         """
         m = len(y)
-        Delta, D, a, delta = np.empty(self.LayersCount-1, dtype=np.ndarray), np.empty(self.LayersCount-1, dtype=np.ndarray), [], [0]*self.LayersCount
+        Delta, a = np.empty(self.LayersCount-1, dtype=np.ndarray), []
         DVec = np.array([])
         for l in range(self.LayersCount-1):
             Delta[l] = np.zeros_like(self.Theta[l])
-            D[l] = np.zeros_like(self.Theta[l])
         for l in self.Layers:
-            a.append(np.zeros(l+1))
-        a[-1] = np.zeros(self.Layers[-1])
+            a.append(np.zeros((l+1, m)))
+        a[-1] = np.zeros((self.Layers[-1], m))
+        a[0] = np.vstack((np.ones((1, m)), X.T))
         a = np.array(a)
+        delta = np.zeros_like(a)
 
-        for i in range(m):
-            a[0] = np.append(1, X[i])
-            # forward propagation
-            for j in range(1, self.LayersCount-1):
-                a[j] = np.append(1, sigmoid(self.Theta[j-1] @  a[j-1]))
-            a[-1] = sigmoid(self.Theta[-1] @ a[-2])
-            # compute delta errors
-            delta[-1] = a[-1] - y[i]
-            for j in range(self.LayersCount-2, 0, -1):
-                delta[j] = ((self.Theta[j].T @ delta[j+1]) * (a[j] * (1-a[j])))[1:]
-            for l in range(self.LayersCount-1):
-                Delta[l] += delta[l+1].reshape(len(delta[l+1]), 1) @ a[l].reshape(len(a[l]), 1).T
-        D = Delta.copy()
+        # forward propagation
+        for j in range(1, self.LayersCount-1):
+            a[j] = np.vstack((np.ones((1, m)), sigmoid(self.Theta[j-1] @  a[j-1])))
+        a[-1] = sigmoid(self.Theta[-1] @ a[-2])
+
+        # backward propagation
+        delta[-1] = a[-1] - y.T
+        for j in range(self.LayersCount-2, 0, -1):
+            delta[j] = ((self.Theta[j].T @ delta[j+1]) * (a[j] * (1-a[j])))[1:, ]
+        for l in range(self.LayersCount-1):
+            Delta[l] += delta[l+1] @ a[l].T
         T = self.Theta.copy()
         for t in T:
             t[:,0] = 0
-        D = (1/m) * D + lmb*T
-        for d in D:
+        Delta = (1/m) * Delta + Regularization*T
+        for d in Delta:
             DVec = np.append(DVec, d)
         return DVec
 
-    def NumGrad(self, X, y, lmb = 0):
+    def NumGrad(self, X, y, Regularization = 0):
         """
         Compute gradient of cost function for inputs matrix X and correct outputs y
         using finite differences.
         X and y must be numpy arrays w/ shapes (m, n) and (m, 1) respectively.
 
-        lmb (float)     - regularization parameter.
+        Regularization (float)     - regularization parameter.
 
         Returned value is in vector form.
         Recommended to use only for debugging purpose because of slow computations.
@@ -126,49 +125,52 @@ class Network:
         for i in range(len(DVec)):
             self.ThetaVec[i] += eps
             self.rollTheta()
-            jPlus = self.Cost(X, y, lmb)
+            jPlus = self.Cost(X, y, Regularization)
             self.ThetaVec[i] -= 2*eps
             self.rollTheta()
-            jMinus = self.Cost(X, y, lmb)
+            jMinus = self.Cost(X, y, Regularization)
             self.ThetaVec[i] += eps
             self.rollTheta()
             DVec[i] = (jPlus - jMinus)/(2*eps)
         return DVec
 
-    def GradDesc(self, X, y, alpha = 0.01, lmb = 0, eps = 1e-4, MaxIter = 400):
+    def GradDesc(self, X, y, LearningRate = 0.01, Regularization = 0, Tolerance = 1e-4, MaxIter = 400, PrintStep = 1):
         """
         Perform gradient descent to fit weights for inputs matrix X and correct outputs y.
         X and y must be numpy arrays w/ shapes (m, n) and (m, 1) respectively.
 
-        alpha (float)   - learning rate.
-        lmb (float)     - regularization parameter.
-        eps (float)     - stopping threshold.
-        MaxIter (int)   - maximum number of iterations.
+        LearningRate (float)        - learning rate.
+        Regularization (float)      - regularization constant.
+        Tolerance (float)           - algorithm stops, when two Cost values differ less.
+        MaxIter (int)               - maximum number of iterations.
+        PrintStep (int)             - step for printing iteration info.
 
         Returns array of values of cost functions at each iteration.
         """
         J_hist = []
         # perform gradient descent, but Theta's are unrolled into vector
         for i in range(MaxIter):
-            self.ThetaVec -= alpha*self.Grad(X, y, lmb)
+            self.ThetaVec -= LearningRate*self.Grad(X, y, Regularization)
             self.rollTheta()
-            print(f"iteration {i+1} \t J = {self.Cost(X, y, lmb)}")
-            J_hist.append(self.Cost(X, y, lmb))
+            if i%PrintStep == 0:
+                print(f"iteration {i} \t J = {self.Cost(X, y, Regularization)}")
+            J_hist.append(self.Cost(X, y, Regularization))
             if i>0:
-                if abs(J_hist[i] - J_hist[i-1]) < eps:
+                if abs(J_hist[i] - J_hist[i-1]) < Tolerance:
                     break
         print("--- GradDesc finished ---")
         return J_hist
 
-    def SGD(self, X, y, BatchSize, alpha = 0.01, eps = 1e-4, MaxIter = 400):
+    def SGD(self, X, y, BatchSize = 1, LearningRate = 0.01, Tolerance = 1e-4, MaxIter = 400, PrintStep = 1):
         """
         Perform stochastic gradients descent to fit weights for inputs matrix X and correct outputs y.
         X and y must be numpy arrays w/ shapes (m, n) and (m, 1) respectively.
 
-        BatchSize (int) - size of data batch used to compute gradient.
-        alpha (float)   - learning rate.
-        eps (float)     - stopping threshold.
-        MaxIter (int)   - maximum number of iterations.
+        LearningRate (float)        - learning rate.
+        BatchSize (float)           - size of mini-batch used to compute gradient.
+        Tolerance (float)           - algorithm stops, when two Cost values differ less.
+        MaxIter (int)               - maximum number of iterations.
+        PrintStep (int)             - step for printing iteration info.
 
         Returns array of values of cost functions at each iteration.
         """
@@ -178,12 +180,13 @@ class Network:
         for i in range(MaxIter):
             np.random.shuffle(X_shuffle)
             for j in range(0, m, BatchSize):
-                self.ThetaVec -= alpha*self.Grad(X[j:j+BatchSize], y[j:j+BatchSize], lmb = 0)
+                self.ThetaVec -= LearningRate*self.Grad(X[j:j+BatchSize], y[j:j+BatchSize], Regularization = 0)
                 self.rollTheta()
-            print(f"iteration {i+1} \t J = {self.Cost(X, y, lmb = 0)}")
-            J_hist.append(self.Cost(X, y, lmb = 0))
+            if i%PrintStep == 0:
+                print(f"iteration {i} \t J = {self.Cost(X, y, Regularization = 0)}")
+            J_hist.append(self.Cost(X, y, Regularization = 0))
             if i>0:
-                if abs(J_hist[i] - J_hist[i-1]) < eps:
+                if abs(J_hist[i] - J_hist[i-1]) < Tolerance:
                     break
         print("--- SGD finished ---")
         return J_hist
